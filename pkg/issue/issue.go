32aa4e687051e142760cbcdb567d923cdb4791b8
Theofilos Petsios <theofilos@crashoverride.com>
2022-09-29 20:10:49 +0300

    added sample webhook auditor

--- pkg/issue/issue.go
+++ pkg/issue/issue.go
@@ -2,16 +2,19 @@ package issue
 
 import (
 	"github.com/crashappsec/github-security-auditor/pkg/issue/category"
+	"github.com/crashappsec/github-security-auditor/pkg/issue/resource"
 	"github.com/crashappsec/github-security-auditor/pkg/issue/severity"
 	"github.com/crashappsec/github-security-auditor/pkg/issue/tags"
 )
 
 type Issue struct {
-	ID          string            `json:"id"`
-	Name        string            `json:"name"`
-	Severity    severity.Severity `json:"severity"`
-	Category    category.Category `json:"category"`
-	Tags        []tags.Tag        `json:"tags,omitempty"`
-	Description string            `json:"description"`
-	Remediation string            `json:"remediation"`
+	ID          string              `json:"id"`
+	Name        string              `json:"name"`
+	Severity    severity.Severity   `json:"severity"`
+	Category    category.Category   `json:"category"`
+	Tags        []tags.Tag          `json:"tags,omitempty"`
+	Description string              `json:"description"`
+	Resources   []resource.Resource `json:"resource"`
+	CWEs        []int               `json:"cwes,omitempty"`
+	Remediation string              `json:"remediation"`
 }
